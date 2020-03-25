037675738bcf462aaa63d280df3e37d98e12fa8c
github username <github email address>
2020-03-25 01:30:04 -0700

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,11 +1,24 @@
 class StudentsController < ApplicationController
+  before_action :set_student, only: :show
   
   def index
     @students = Student.all
   end
 
   def show
-    @student = Student.find(params[:id])
   end
 
+  def new
+  end
+
+  def create
+    session[:new_student_params] = params
+    redirect_to new_student_path
+  end
+
+  private
+
+    def set_student
+      @student = Student.find(params[:id])
+    end
 end
